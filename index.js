acdc2535e59b178409f7bcd9bceeb86142320902
Bebraeshkrere <144522850+Bebraeshkrere@users.noreply.github.com>
2024-04-06 18:28:19 +1000

    Add files via upload

---
+++ index.js
@@ -0,0 +1,13 @@
+let block = document.querySelectorAll('.block')
+console.log(block)
+
+
+for(let i = 0; i < block.length; i++) {
+    block[i].addEventListener('mouseenter', function(){
+    console.log('qwerty', i);
+    block[i].classList.add('--active');
+    setTimeout(() => {
+        block[i].classList.remove('--active');
+    }, 1000);
+})
+}
